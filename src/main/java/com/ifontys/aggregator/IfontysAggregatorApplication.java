c5535747d172b2c0aaaf9ff4eb51ecacdaa668ae
Ruben <Ruben@netwerk-c.nl>
2022-06-02 10:46:22 +0200

    Initial commit
    Initial commit

---
+++ src/main/java/com/ifontys/aggregator/IfontysAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.ifontys.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfontysAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfontysAggregatorApplication.class, args);
+    }
+
+}
