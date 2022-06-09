1bbe773c835bf1b854b8ddc2b6aa98d2ad456ee9
Ruben <Ruben@Leerentveld.net>
2022-06-09 15:01:20 +0200

    made aggregation dynamic

--- src/main/java/com/ifontys/aggregator/logic/Aggregator.java
+++ src/main/java/com/ifontys/aggregator/logic/Aggregator.java
@@ -1,56 +1,72 @@
 package com.ifontys.aggregator.logic;
 
+import com.rabbitmq.client.Command;
 import org.apache.tomcat.util.json.JSONParser;
 
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Component
 public class Aggregator {
 
-    //TODO: The problem here is extensibility, if more instances appear.. we do have a problem.
+    JSONObject aggregatedData= new JSONObject();
+    JSONObject data = new JSONObject();
 
     public void AggregateResults(String result){
-        JSONObject aggregatedData = new JSONObject();
-        JSONObject jsonResult = new JSONObject(result);
+        data = new JSONObject(result);
 
-        aggregatedData.put("pcn", jsonResult.getJSONObject("canvas").getString("id"));
-        aggregatedData.put("name", jsonResult.getJSONObject("canvas").getString("givenName"));
-        aggregatedData.put("surname", jsonResult.getJSONObject("canvas").getString("surName"));
-        aggregatedData.put("displayName", jsonResult.getJSONObject("canvas").getString("displayName"));
-        aggregatedData.put("email", jsonResult.getJSONObject("canvas").getString("mail"));
-        aggregatedData.put("office", jsonResult.getJSONObject("canvas").getString("office"));
-        aggregatedData.put("officeTelephone", jsonResult.getJSONObject("canvas").getString("telephoneNumber"));
-        aggregatedData.put("mobileTelephone", jsonResult.getJSONObject("canvas").getString("mobileNumber"));
-        aggregatedData.put("photo", jsonResult.getJSONObject("canvas").getString("photo"));                     //Willen we echt een foto bij tus hebben?
-        aggregatedData.put("department", jsonResult.getJSONObject("canvas").getString("department"));
-        aggregatedData.put("groups", jsonResult.getJSONObject("canvas").getJSONArray("groups"));
-        aggregatedData.put("affiliations", jsonResult.getJSONObject("canvas").getJSONArray("affiliations"));
-        aggregatedData.put("canvasUid", jsonResult.getJSONObject("canvas").getString("uid"));
-        aggregatedData.put("employeeId", jsonResult.getJSONObject("canvas").getString("employeeId"));
-
-        aggregatedData.put("facebook", jsonResult.getJSONObject("sharepoint").getString("facebook"));
-        aggregatedData.put("linkedIn", jsonResult.getJSONObject("sharepoint").getString("linkedIn"));
-        aggregatedData.put("twitter", jsonResult.getJSONObject("sharepoint").getString("twitter"));
-        aggregatedData.put("schooling", jsonResult.getJSONObject("sharepoint").getJSONArray("schools"));
-        aggregatedData.put("pastEmployers", jsonResult.getJSONObject("sharepoint").getJSONArray("pastEmployers"));
-        aggregatedData.put("currentProjects", jsonResult.getJSONObject("sharepoint").getJSONArray("currentProjects"));
-        aggregatedData.put("ambitions", jsonResult.getJSONObject("sharepoint").getJSONArray("ambitions"));
-        aggregatedData.put("contributions", jsonResult.getJSONObject("sharepoint").getString("contributions"));
-        aggregatedData.put("goals", jsonResult.getJSONObject("sharepoint").getString("goals"));
-        aggregatedData.put("schedule", jsonResult.getJSONObject("sharepoint").getString("schedule"));
-        aggregatedData.put("projectDescriptions", jsonResult.getJSONObject("sharepoint").getString("projectDescriptions"));
-        aggregatedData.put("aboutMe", jsonResult.getJSONObject("sharepoint").getString("aboutMe"));
-        aggregatedData.put("Interests", jsonResult.getJSONObject("sharepoint").getJSONArray("interests"));
-        aggregatedData.put("responsibilities", jsonResult.getJSONObject("sharepoint").getJSONArray("responsibilities"));
-        aggregatedData.put("skills", jsonResult.getJSONObject("sharepoint").getJSONArray("skills"));
-        aggregatedData.put("title", jsonResult.getJSONObject("sharepoint").getString("title"));
+        PutProperty("pcn", "canvas","id", String.class);
+        PutProperty("name", "canvas", "givenName", String.class);
+        PutProperty("surname", "canvas", "surName", String.class);
+        PutProperty("displayName", "canvas", "displayName", String.class);
+        PutProperty("email", "canvas", "mail", String.class);
+        PutProperty("office", "canvas", "office", String.class);
+        PutProperty("officeTelephone", "canvas", "telephoneNumber", String.class);
+        PutProperty("mobileTelephone", "canvas", "mobileNumber", String.class);
+        PutProperty("photo", "canvas", "photo", String.class);
+        PutProperty("department", "canvas", "department", String.class);                   //Willen we echt een foto bij tus hebben?
+        PutProperty("groups", "canvas", "groups", ArrayList.class);
+        PutProperty("affiliations", "canvas", "affiliations", ArrayList.class);
+        PutProperty("canvasUid", "canvas", "uid", String.class);
+        PutProperty("employeeId", "canvas", "employeeId", String.class);
 
 
+        PutProperty("facebook", "sharepoint", "facebook", String.class);
+        PutProperty("linkedIn", "sharepoint", "linkedIn", String.class);
+        PutProperty("twitter", "sharepoint", "twitter", String.class);
+        PutProperty("schooling", "sharepoint", "schools", ArrayList.class);
+        PutProperty("pastEmployers", "sharepoint", "pastEmployers", ArrayList.class);
+        PutProperty("currentProjects", "sharepoint", "currentProjects", ArrayList.class);
+        PutProperty("ambitions", "sharepoint", "ambitions", ArrayList.class);
+        PutProperty("contributions", "sharepoint", "contributions", String.class);
+        PutProperty("goals", "sharepoint", "goals", String.class);
+        PutProperty("schedule", "sharepoint", "schedule", String.class);
+        PutProperty("projectDescriptions", "sharepoint", "projectDescriptions", String.class);
+        PutProperty("aboutMe", "sharepoint", "aboutMe", String.class);
+        PutProperty("interests", "sharepoint", "interests", ArrayList.class);
+        PutProperty("responsibilities", "sharepoint", "responsibilities", ArrayList.class);
+        PutProperty("skills", "sharepoint", "skills", ArrayList.class);
+        PutProperty("title", "sharepoint", "title", String.class);
+      
 
         System.out.println("[X] Aggregatred data: " + aggregatedData);
+    }
+
+    public void PutProperty(String newName, String source, String name, Class<?> type){
+        try{
+            if(type == String.class){
+                aggregatedData.put(newName, data.getJSONObject(source).getString(name));
+            }else if(type == ArrayList.class){
+                aggregatedData.put(newName, data.getJSONObject(source).getJSONArray(name));
+            }
+        } catch (Exception e){
+
+        }
     }
 
 
